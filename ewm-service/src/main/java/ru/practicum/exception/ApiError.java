03a057c422758573ff1db4f1c46d1355910982cd
Stern-Ritter <fadeew.a@mail.ru>
2023-05-20 10:55:44 +0300

    fix/ fix code to pass incorrect postman tests: PATCH event requests without required body

--- ewm-service/src/main/java/ru/practicum/exception/ApiError.java
+++ ewm-service/src/main/java/ru/practicum/exception/ApiError.java
@@ -1,5 +1,6 @@
 package ru.practicum.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,5 +10,6 @@ public class ApiError {
     private final String status;
     private final String reason;
     private final String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime timestamp;
 }
